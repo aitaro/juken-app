72279fe09ae5a65010dd7e317b5498654468b09e
Aitaro Chaya <aitaro-123@hotmail.co.jp>
2018-02-25 11:54:42 +0900

    Complete edit session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :name, presence: true
-  validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password,
-    presence: true,
-    length: {minimum: 6}
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+    # 渡された文字列のハッシュ値を返す
+    def self.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
